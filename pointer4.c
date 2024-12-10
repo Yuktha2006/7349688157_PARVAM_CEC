e15b1b30bbfa9ce83317c1e2138ba42000761809
Yuktha2006 <yukthae2006@gmail.com>
2024-12-10 12:19:33 +0530

    Add files via upload

---
+++ pointer4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int size=0,a[100],i,*large=a;
+	scanf("%d",&size);
+	for( i=0;i<size;scanf("%d",a+i++));
+    
+    for(i=1;i<size;i++)
+    {
+        if(*large<a[i])
+        large=&a[i];
+    }
+    printf("largest element is %d",*large);
+    
+}
